b70f95c98ca479f0122478910ba125ce9fe78385
Thushan Vimukthi <thushanjayasundara96@gmail.com>
2025-11-24 20:45:02 +0200

    Feature: Add Morgan middleware for HTTP request logging

--- week5/week5-be-pp1/app.js
+++ week5/week5-be-pp1/app.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
+// Import the router (instead of individual functions)
 const tourRouter = require("./routes/tourRouter.js");
 const userRouter = require("./routes/userRouter.js");
 
 app.use(express.json());
+//app.use(morgan("tiny"));
+app.use(morgan("dev"));
 
-app.use("/api/tours", tourRouter); // ← Changed: added /api
-app.use("/api/users", userRouter); // ← Changed: added /api
+// Use the router for all /tours routes
+app.use("/api/tours", tourRouter);
+app.use("/api/users", userRouter);
+
+const port = 4000;
+app.listen(port, () => console.log(`Server on port ${port}`));
