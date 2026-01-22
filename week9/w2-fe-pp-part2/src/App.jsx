7c33df9f9c2864381d80260bdeb69276375050a6
Thushan Vimukthi <thushanjayasundara96@gmail.com>
2026-01-22 12:56:03 +0200

    complete Part 3/5: CreditCard

--- week9/w2-fe-pp-part2/src/App.jsx
+++ week9/w2-fe-pp-part2/src/App.jsx
@@ -1,9 +1,48 @@
 import "./App.css";
+import CreditCard from "./CreditCard"
 
 function App() {
   return (
     <div className="App">
       <h1> LAB | React Training</h1>
+
+        <div className="cards-row">
+        <CreditCard
+            type="Visa"
+            number="0123456789018875"
+            expirationMonth={3}
+            expirationYear={2021}
+            bank="BNP"
+            owner="Maxence Bouret"
+            bgColor="#11aa99"
+            color="white"
+        />
+
+        <CreditCard
+            type="Master Card"
+            number="0123456789010993"
+            expirationMonth={3}
+            expirationYear={2021}
+            bank="N26"
+            owner="Maxence Bouret"
+            bgColor="#eeeeee"
+            color="#222222"
+        />
+
+        <CreditCard
+            type="Visa"
+            number="0123456789016982"
+            expirationMonth={12}
+            expirationYear={2019}
+            bank="Name of the Bank"
+            owner="Firstname Lastname"
+            bgColor="#ddbb55"
+            color="white"
+        />
+        </div>
+
+
+
     </div>
   );
 }
