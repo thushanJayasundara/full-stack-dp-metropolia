fd6cd84240a827a21618135fcad64f63c71764ae
Thushan Vimukthi <thushanjayasundara96@gmail.com>
2026-02-26 09:51:08 +0200

    feat(add-book): send POST request from AddBookPage form

--- week14/week6-fepp-option-b/frontend/src/pages/AddBookPage.jsx
+++ week14/week6-fepp-option-b/frontend/src/pages/AddBookPage.jsx
@@ -1,3 +1,13 @@
+const [title, setTitle] = useState("");
+const [author, setAuthor] = useState("");
+const [isbn, setIsbn] = useState("");
+const [publisher, setPublisher] = useState("");
+const [genre, setGenre] = useState("");
+const [isAvailable, setIsAvailable] = useState("true");
+const [dueDate, setDueDate] = useState("");
+const [borrower, setBorrower] = useState("");
+
+
 const AddBookPage = () => {
   const submitForm = (e) => {
     e.preventDefault();
@@ -8,25 +18,50 @@ const AddBookPage = () => {
     <div className="create">
       <h2>Add a New Book</h2>
       <form onSubmit={submitForm}>
+
         <label>Book Title:</label>
-        <input type="text" required />
+        <input type="text" required
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}/>
+
         <label>Author:</label>
-        <input type="text" required />
+        <input type="text" required
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}/>
+
         <label>ISBN:</label>
-        <input type="text" required />
+        <input type="text" required
+        value={isbn}
+        onChange={(e) => setIsbn(e.target.value)}/>
+
         <label>Publisher:</label>
-        <input type="text" required />
+        <input type="text" required
+        value={publisher}
+        onChange={(e) => setPublisher(e.target.value)}/>
+
         <label>Genre:</label>
-        <input type="text" required />
+        <input type="text" required
+        value={genre}
+        onChange={(e) => setGenre(e.target.value)}/>
+
         <label>Available:</label>
-        <select>
+        <select
+        value={isAvailable}
+        onChange={(e) => setIsAvailable(e.target.value === "true")}>
           <option value="true">Yes</option>
           <option value="false">No</option>
         </select>
+
         <label>Due Date:</label>
-        <input type="date" />
+        <input type="date"
+        value={dueDate}
+        onChange={(e) => setDueDate(e.target.value)}/>
+
         <label>Borrower:</label>
-        <input type="text" />
+        <input type="text"
+        value={borrower}
+        onChange={(e) => setBorrower(e.target.value)}/>
+
         <button>Add Book</button>
       </form>
     </div>
